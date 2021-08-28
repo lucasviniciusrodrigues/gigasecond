f4266e9d7e21ce421591bcef743ec0742e7340b0
lucasviniciusrodrigues <lucas.vinicius.rodrigues@hotmail.com>
2021-08-28 14:35:07 -0300

    First commit

---
+++ src/main/java/Gigasecond.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Gigasecond {
+
+    private static long GIGASECONDS = 1000000000L;
+    LocalDateTime dateTime;
+
+    public Gigasecond(LocalDate moment) {
+        dateTime = moment.atTime(LocalTime.MIDNIGHT);
+    }
+
+    public Gigasecond(LocalDateTime moment) {
+        dateTime = moment;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime.plusSeconds(GIGASECONDS);
+    }
+}
